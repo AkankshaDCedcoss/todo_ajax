48051a6d02c3b9e7fe9a27a383c3f53bfef6aaf1
AkankshaDCedcoss <akankshadwivedi@cedcommerce.com>
2022-03-14 20:09:51 +0530

    done

--- src/post.php
+++ src/post.php
@@ -1,147 +1,31 @@
 <?php
 session_start();
-// session_destroy();
+if(!isset( $_SESSION['todo'])){
+    $_SESSION['todo'] = array();
+}
+if(!isset( $_SESSION['complete'])){
+    $_SESSION['complete'] = array();
+}
+?>
+<?php
+if(isset($_POST['action']) && $_POST['action']=='todo'){
+        array_push( $_SESSION['todo'] ,$_POST['todo']);
 
-
-if(!isset($_SESSION['todoComplete']))
-{
-    $_SESSION['todoComplete']=array();
+        echo json_encode($_SESSION['todo']);
+}
+if(isset($_POST['action']) && $_POST['action']=='edit'){
+    array_splice($_SESSION['todo'],$_POST['edit'],1,$_POST['u_value']);
+       echo json_encode($_SESSION['todo']);
+}
+if(isset($_POST['action']) && $_POST['action']=='delete'){
+    $a = $_POST['delete'];
+    array_splice($_SESSION['todo'],$_POST['delete'],1);
+       echo json_encode($_SESSION['todo']);
+}
+if(isset($_POST['action']) && $_POST['action']=='check'){
+    array_splice($_SESSION['todo'],$_POST['delete'],1);
+    array_push($_SESSION['complete'] ,$_POST['complete']);
+       echo json_encode($_SESSION['complete']);
 }
 
-
-
-
-
-if(isset($_POST))
-{
-    
-    $addtodo=$_POST['addtodo'];
-    $action=$_POST['action'];
-$key1=$_POST['key1'];
-$key2=$_POST['key2'];
-$key3=$_POST['key3'];
-$key4=$_POST['key4'];
-    switch($action)
-    {
-case 'addData':
-{
-add($addtodo);
-}
-break;
-
-case 'complete':
-    {
-    completeTo($key1);
-    }
-    break;
-
-    case 'update':
-        {
-        updateTo($key2,$key3);
-        }
-        break;
-
-        case 'delete':
-            {
-            deleteTo($key4);
-            }
-            break;
-
-
-
-
-
-
-
-
-
-
-    }
-
-
-
-
-
-
-}
-
-
-
-
-function add($addtodo){
-
-
-    if(!isset($_SESSION['todoAjax']))
-    {
-        $_SESSION['todoAjax']=array();
-    }
-    
-    
-
-    $x=$addtodo;
- 
-    array_push($_SESSION['todoAjax'],$x);
-    echo json_encode($_SESSION['todoAjax']);
-    
-
-}
-
-
-
-function completeTo($key1)
-{
-
-    for($i=0;$i < count($_SESSION['todoAjax']);$i++)
-    {
-        if($i == $key1)
-        {
-
-array_push($_SESSION['todoComplete'],$_SESSION['todoAjax'][$i]);
-array_splice($_SESSION['todoAjax'],$i,1);
-echo json_encode($_SESSION['todoComplete']);
-
-        }
-    }
-
-}
-
-
-function updateTo($key2,$key3)
-{
-
-    for($i=0;$i < count($_SESSION['todoAjax']);$i++)
-    {
-        if($i == $key2)
-        {
-
-
-array_splice($_SESSION['todoAjax'],$i,1,$key3);
-echo json_encode($_SESSION['todoAjax']);
-
-        }
-    }
-
-
-
-}
-
-
-
-function deleteTo($key4)
-{
-
-
-    for($i=0;$i < count($_SESSION['todoAjax']);$i++)
-    {
-        if($i == $key4)
-        {
-
-
-array_splice($_SESSION['todoAjax'],$i,1);
-echo json_encode($_SESSION['todoAjax']);
-
-        }
-    }
-
-
-}
+?>
--- src/todo.php
+++ src/todo.php
@@ -1,242 +1,194 @@
 <?php
-include 'post.php';
 session_start();
-// session_destroy();
-
+//session_destroy();
 ?>
-<html>
-
+<!DOCTYPE html>
+<html lang="en">
 <head>
-    <title>TODO List</title>
-    <link href="style.css" type="text/css" rel="stylesheet">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script>
-        var arr = [];
-        var data;
-        $(document).ready(function() {
-            $("#add").click(function()
-
-                {
-
-
-
-                    var value = $("#newtask").val();
-                    console.log(value);
-
-
-                    $.ajax({
-                        url: "post.php",
-                        type: "POST",
-                        data: {
-                            addtodo: value,
-                            action: 'addData'
-                        },
-                        dataType: 'json',
-                        success: function(data1) {
-                            arr = data1;
-                            display1(arr);
-
-
-                        }
-                    });
-                });
-
-        });
-
-
-        function display1(arr2) {
-            data = arr2.length;
-            var html = '<table>';
-            for (var i = 0; i < arr2.length; i++) {
-
-
-                html += "<tr><td><input type='checkbox' id='" + i + "' onclick='compleated(" + i + ")'></td><td>" + arr2[i] + "</td><td><input type='text'></td><td><button onclick='edit(" + i + ")' >Edit</button></td><td><button onclick='deleteThis(" + i + ")' >Delete</button></td></tr>";
-            }
-            html += "</table>";
-            document.getElementById("result1").innerHTML = html;
-            console.log("data is" + arr[0]);
-        }
-
-
-
-
-
-        function compleated(x) {
-
-
-            $.ajax({
-                url: "post.php",
-                type: "POST",
-                data: {
-                    key1: x,
-                    action: 'complete'
-                },
-                dataType: 'json',
-                success: function(data2) {
-
-                    var arr9 = data2;
-                    display2(arr9);
-                    display3(arr9, x);
-                    console.log(arr2);
-
-                }
-
-
-
-
-            });
-
-
-
-
-        }
-
-        function edit(x) {
-            for (var i = 0; i < data; i++) {
-                if (i == x) {
-                    $("#newtask").val(arr[i]);
-                    var y = i;
-                }
-            }
-
-
-            $("#update").on("click", function() {
-                var editTask = $("#newtask").val();
-                console.log(editTask);
-                console.log(x);
-
-
-
-                $.ajax({
-                    url: "post.php",
-                    type: "POST",
-                    data: {
-                        key2: y,
-                        key3: editTask,
-
-                        action: 'update'
-                    },
-                    dataType: 'json',
-                    success: function(data3) {
-
-                        var arrNew = data3;
-                        display1(arrNew);
-                        console.log(arrNew);
-
-
-                    }
-
-
-
-
-                });
-
-
-
-
-            })
-
-
-
-
-
-
-
-
-
-        }
-
-
-
-
-        function deleteThis(x) {
-
-            $.ajax({
-                url: "post.php",
-                type: "POST",
-                data: {
-                    key4: x,
-
-
-                    action: 'delete'
-                },
-                dataType: 'json',
-                success: function(data4) {
-
-                    var arrDelet = data4;
-                    display1(arrDelet);
-                    console.log(arrDelet);
-
-
-                }
-
-
-
-
-            });
-
-
-
-
-
-
-        }
-
-
-        function display2(arr3) {
-            var html1 = '<table>';
-            for (var i = 0; i < arr3.length; i++) {
-
-
-                html1 += "<tr><td><input type='checkbox' id='" + i + "' onclick='compleated(" + i + ")'></td><td>" + arr2[i] + "</td><td><input type='text'></td><td><button onclick='edit(" + i + ")' >Edit</button></td><td><button onclick='deleteThis(" + i + ")' >Delete</button></td></tr>";
-            }
-            html1 = "</table>";
-            document.getElementById("result2").innerHTML = html1;
-        }
-
-
-
-        function display3(arr, x) {
-            var html2 = '<table>';
-            for (var i = 0; i < arr.length; i++) {
-                if (x == i) {
-                    arr.splice(i, 1);
-                    continue;
-                }
-
-                html2 += "<tr><td><input type='checkbox' id='" + i + "' onclick='compleated(" + i + ")'></td><td>" + arr2[i] + "</td><td><input type='text'></td><td><button onclick='edit(" + i + ")' >Edit</button></td><td><button onclick='deleteThis(" + i + ")' >Delete</button></td></tr>";
-            }
-            html2 = "</table>";
-            document.getElementById("result").innerHTML = html2;
-        }
-    </script>
+    <title>todo</title>
 </head>
-
 <body>
-    <div class="container">
-        <h2>TODO LIST</h2>
-        <h3>Add Item</h3>
-        <p>
-            <input id="newtask" type="text">
-            <button id="add">Add</button>
-            <button id="update">update</button>
-        </p>
-
+    <h2>TODO LIST</h2>
+    <h3>Add Item</h3>
+    <input type="text" id="input">
+    <button type="submit" id="btn1" >Add</button>
+    <button type="submit" id="btn2" >Update</button>
+    <div> 
         <h3>Todo</h3>
-        <ul id="incomplete-tasks">
-            <li><input type="checkbox"><label>Pay Bills</label><input type="text"><button class="edit">Edit</button><button class="delete">Delete</button></li>
-            <div class="div2" id="result1"></div>
-            <li><input type="checkbox"><label>Go Shopping</label><input type="text" value="Go Shopping"><button class="edit">Edit</button><button class="delete">Delete</button></li>
-        </ul>
-
-        <h3>Completed</h3>
-        <ul id="completed-tasks">
-            <li><input type="checkbox" checked><label>See the Doctor</label><input type="text"><button class="edit">Edit</button><button class="delete">Delete</button></li>
-            <div class="div2" id="result2"></div>
-
-        </ul>
+        <div id="todo"></div>
     </div>
-
+    <div> 
+        <h3>Completed</h3>
+        <div id="completed"></div>
+    </div>
+    <script src="script.js" ></script>
 </body>
-
 </html>
+<script>
+    var length;
+    var js_array = [];
+    var array_diff = [];
+    $(document).ready( function() {
+        $('#btn2').hide();
+       $('#btn1').on('click' , function() {
+          var todo = $('#input').val();
+          if(todo == "") {
+              alert('please enter some value');
+          } else {
+                 $.ajax({
+                     url: 'post.php',
+                     type: 'post',
+                     datatype: 'json',
+                     data: {
+                         action: 'todo',
+                         todo: todo
+                     },
+                     success:function(data){
+                        js_array = JSON.parse(data);
+                        length = js_array.length;
+                         display(js_array);
+                     }
+
+                  })
+          }
+       })
+    })
+    var u_id;
+    function Edit(id){
+        $('#btn1').hide();
+        $('#btn2').show();
+        for (var i = 0; i < length; i++) {
+             if(i == id){
+                u_id = i
+                $('#input').val(js_array[i]);
+                  $('#btn2').on('click' ,function() {
+                    $('#btn2').hide();
+                    $('#btn1').show();
+                      var u_value = $('#input').val();
+                    $.ajax({
+                     url: 'post.php',
+                     type: 'post',
+                     datatype: 'json',
+                     data: {
+                         action: 'edit',
+                         u_value: u_value,
+                         edit: u_id
+                     },
+                     success:function(data){
+                       var js_array = JSON.parse(data);
+                       display(js_array);
+                   }
+                  })
+                })
+             }
+        }
+    }
+    function Delete(id){
+        for (var i = 0; i < length; i++) {
+             if(i == id){
+                $.ajax({
+                     url: 'post.php',
+                     type: 'post',
+                     datatype: 'json',
+                     data: {
+                         action: 'delete',
+                         delete: i
+                     },
+                     success:function(data){
+                        var js_array = JSON.parse(data);
+                        display(js_array);
+                   }
+             })
+        }
+      }
+    }
+    function change(x) {
+        for (var i = 0; i < length; i++) {
+		    if (i == x) {
+                var checkbox = document.getElementById('check' + [ i ]);
+                if(checkbox.checked){
+                    $.ajax({
+                     url: 'post.php',
+                     type: 'post',
+                     datatype: 'json',
+                     data: {
+                         action: 'check',
+                         complete: js_array[i],
+                         check: i
+                     },
+                     success:function(data){
+                        var js_array1 = JSON.parse(data);
+                        var html = '<ul>';
+	                    for (var j = 0; j < js_array.length; j++) {
+                            if(x == j){
+                                js_array.splice(j,1);
+                            
+                                continue;
+                            } else {
+		                        html +=
+			                        '<li><input type ="checkbox" id="check' +
+			                        [ j ] +
+			                        '" onchange="change(' +
+			                        j +
+			                         ')">' +
+			                        js_array[j] +
+			                        ' <button id="Edit" onclick="Edit(' +
+			                        j +
+			                        ')">Edit</button> <button id="Delete" onclick="Delete(' +
+			                        j +
+			                        ')">Delete</button><br> ';
+	
+	                            html += '</li></ul>';
+	                            $('#todo').html(html);
+                            }
+                        }
+                        var html = '<ul>';
+	                       for (var i = 0; i < js_array1.length; i++) {
+		                      html +=
+			                       '<li><input type ="checkbox" id="check' +
+			                        [ i ] +
+			                        '" onchange="change(' +
+			                         i +
+			                         ')" checked>' +
+			                         js_array1[i] +
+			                          ' <button id="Edit" onclick="Edit(' +
+			                         i +
+			                         ')">Edit</button> <button id="Delete" onclick="Delete(' +
+			                         i +
+			                        ')">Delete</button><br> ';
+	                                }
+	                    html += '</li></ul>';
+	                    $('#completed').html(html);
+                        
+                   }
+                })
+            }
+        }
+      }
+    }
+     function display(todolist) {
+
+        var html = '<ul>';
+	       for (var i = 0; i < todolist.length; i++) {
+		      html +=
+			      '<li><input type ="checkbox" id="check' +
+			      [ i ] +
+			      '" onchange="change(' +
+			       i +
+			      ')">' +
+			      todolist[i] +
+			      ' <button id="Edit" onclick="Edit(' +
+			       i +
+			      ')">Edit</button> <button id="Delete" onclick="Delete(' +
+			       i +
+			      ')">Delete</button><br> ';
+	            }
+	      html += '</li></ul>';
+	      $('#todo').html(html);
+
+    }
+</script>
